6bcc12f005ed11263c8b807f3ac59d3251b554ae
Aimé Melançon <aimemelancon@gmail.com>
2026-02-12 10:03:01 -0500

    Update backend/app/db/models/rule.py
    Co-authored-by: Kotchikpa Guy-Landry Allagbe <122434720+k-allagbe@users.noreply.github.com>

--- backend/app/db/models/rule.py
+++ backend/app/db/models/rule.py
@@ -16,12 +16,12 @@ if TYPE_CHECKING:
 class Rule(Base, table=True):
     id: UUID = Field(default_factory=uuid4, primary_key=True)
     reference_number: str = Field(unique=True, index=True, max_length=255)
-    title_en: str = Field()
-    title_fr: str = Field()
-    description_en: str = Field()
-    description_fr: str = Field()
-    url_en: str | None = Field(default=None)
-    url_fr: str | None = Field(default=None)
+    title_en: str
+    title_fr: str
+    description_en: str
+    description_fr: str
+    url_en: str | None = None
+    url_fr: str | None = None
     created_at: datetime = Field(
         default_factory=lambda: datetime.now(UTC),
         sa_column=Column(DateTime(timezone=True), default=func.now()),
