a30e2532221b98f4b70fb5176db5dbdb11357487
Aimé Melançon <aimemelancon@gmail.com>
2026-03-06 19:39:39 +0000

    correction of the regex

--- backend/app/schemas/type/registration_number.py
+++ backend/app/schemas/type/registration_number.py
@@ -1,28 +1,15 @@
 """Schema for registration number type."""
 
-from typing import Annotated, Literal
+from typing import Annotated
 
-from pydantic import BeforeValidator, StringConstraints
+from pydantic import StringConstraints
 
-
-def _spaces_to_empty(v: str) -> str:
-    if isinstance(v, str) and v.strip() == "":
-        return ""
-    return v
-
-
-EmptyRegistrationNumber = Literal[""]
-NonEmptyRegistrationNumber = Annotated[
+RegistrationNumber = Annotated[
     str,
     StringConstraints(
-        pattern=r"^[0-9]{7}[FfPpGgHhMmSs]$",
+        pattern=r"^([0-9]{7}[FfPpGgHhMmSs])?$",
         strict=True,
         strip_whitespace=True,
         to_upper=True,
     ),
-]
-
-RegistrationNumber = Annotated[
-    EmptyRegistrationNumber | NonEmptyRegistrationNumber,
-    BeforeValidator(_spaces_to_empty),
 ]
